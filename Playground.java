0fa4bf396e52b2d07fdf79bb765779669eaf64bf
Pablo <dave@wltcode.com>
2025-01-08 18:52:00 +0200

    Kata's continues

--- Playground.java
+++ Playground.java
@@ -14,14 +14,24 @@ public class Playground {
 
         //Trying out nested if statements
 
-        int a = 5;
-        int b = 4;
+        // int a = 5;
+        // int b = 4;
 
-        if (a > 0) {
-            if (b > a) {
-                System.out.println("B is greater than A");
-            }
-             System.out.println("Outside if statement");
-        }
+        // if (a > 0) {
+        //     if (b > a) {
+        //         System.out.println("B is greater than A");
+        //     }
+        //      System.out.println("Outside if statement");
+        // }
+
+        //Playing with the ternary operator
+
+        System.out.println("This is how we use ternary operator, extension");
+
+        int score = 85;
+
+        char grade = (score >= 90)? 'A': (score >= 80)? 'B': (score >= 70)? 'C': (score >= 60)? 'D': (score >= 50)? 'E': 'F';
+
+        System.out.println("You have attained the following symbol for this class: " + grade);
     }
 }
