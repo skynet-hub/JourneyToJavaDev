3ffa2a4ce021856f996cf28d62ad4e756c3b5c23
Magobo <malesaojhb024@student.wethinkcode.co.za>
2025-08-24 22:56:43 +0200

    key events

--- KeyEvents/src/main/java/dev/magobolesaomako/keyevents/HelloApplication.java
+++ KeyEvents/src/main/java/dev/magobolesaomako/keyevents/HelloApplication.java
@@ -3,6 +3,7 @@ package dev.magobolesaomako.keyevents;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,10 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        scene.setOnKeyPressed(keyEvent -> {
+            HelloController controller = new HelloController();
+            controller.onKeyPress(keyEvent);
+        });
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- KeyEvents/src/main/java/dev/magobolesaomako/keyevents/HelloController.java
+++ KeyEvents/src/main/java/dev/magobolesaomako/keyevents/HelloController.java
@@ -2,6 +2,9 @@ package dev.magobolesaomako.keyevents;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.input.KeyEvent;
+
+import static javafx.scene.input.KeyCode.UP;
 
 public class HelloController {
     @FXML
@@ -10,5 +13,16 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void onKeyPress(KeyEvent event){
+        switch (event.getCode()){
+            case UP:
+                System.out.println("Up key has been pressed");
+                break;
+            default:
+                System.out.println("This key has not been handled");
+        }
+
     }
 }
