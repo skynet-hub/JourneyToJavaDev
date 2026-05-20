1891748140399a88214ee4708b374c2d352e5c06
Magobo <davidlesaomako@gmail.com>
2026-05-20 23:30:10 +0200

    Additions(task dialog): Window to add tasks

---
+++ TodoApp/src/main/java/dev/magobolesaomako/todoapp/controller/TaskAddDialogController.java
@@ -0,0 +1,16 @@
+package dev.magobolesaomako.todoapp.controller;
+
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+
+public class TaskAddDialogController {
+    public MFXTextField taskTitleField;
+    public TextArea taskDescriptionField;
+
+    public void handleCancel(ActionEvent actionEvent) {
+    }
+
+    public void handleSubmit(ActionEvent actionEvent) {
+    }
+}
