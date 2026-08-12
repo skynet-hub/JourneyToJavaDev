97ebf44eebc2d8c66088846cbfe182dc5c193faa
Magobo <davidlesaomako@gmail.com>
2026-08-12 23:55:27 +0200

    Start simulation

--- SmartHomeSystem/src/main/java/com/smarthome/devices/Main.java
+++ SmartHomeSystem/src/main/java/com/smarthome/devices/Main.java
@@ -1,17 +1,21 @@
 package com.smarthome.devices;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        PowerSource battery = new PowerSource("Battery");
+        PowerSource Electric = new PowerSource("Electric");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        SmartLight light = new SmartLight(battery);
+        SmartThermostat thermostat = new SmartThermostat(Electric);
+
+        WifiModule wifiLight = new WifiModule("Wifi_Light");
+        WifiModule wifiThermostat = new WifiModule("Wifi_Thermostat");
+
+        light.setWifiModule(wifiLight);
+        thermostat.setWifiModule(wifiThermostat);
+
+        //Show the details
+        light.showDeviceDetails();
+        thermostat.showDeviceDetails();
     }
 }
